7d43e3e7543f6b10c8384616a5a8bcadcf30c3bb
jordy-u <16016998@student.hhs.nl>
2019-02-14 17:46:22 +0100

    Some text was not added somehow.

--- Software/functionset.js
+++ Software/functionset.js
@@ -8,8 +8,3 @@ module.exports = {
    return reversedString;
 }
 };
-
-//https://stackoverflow.com/questions/1431094/how-do-i-replace-a-character-at-a-particular-index-in-javascript
-String.prototype.replaceAt=function(index, replacement) {
-    return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
-}

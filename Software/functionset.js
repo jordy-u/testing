d2e6c72dab39de8d728cb8e0104a4fcd4a724262
jordy-u <16016998@student.hhs.nl>
2019-02-14 17:44:00 +0100

    String reverser completed

--- Software/functionset.js
+++ Software/functionset.js
@@ -1,7 +1,15 @@
 module.exports = {
   reverseString: function (stringToReverse) {
-   return stringToReverse;
+	  var sizeOfString = stringToReverse.length;
+	  var reversedString = "";
+	  for (var i=0; i<sizeOfString; i++) {
+		  reversedString += stringToReverse[sizeOfString-i-1];
+	  }
+   return reversedString;
 }
 };
 
-
+//https://stackoverflow.com/questions/1431094/how-do-i-replace-a-character-at-a-particular-index-in-javascript
+String.prototype.replaceAt=function(index, replacement) {
+    return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
+}
--- Software/main.js
+++ Software/main.js
@@ -1,5 +1,20 @@
+/* Refferenties:
+
+
+*/
+
 console.log("begin");
 var fs = require('./functionset');
-var reversedString = fs.reverseString("testy");
-console.log(reversedString);
-console.log("eind");
+
+// Get process.stdin as the standard input object.
+var standard_input = process.stdin;
+// Set input character encoding.
+standard_input.setEncoding('utf-8');
+// Prompt user to input data in console.
+console.log("Please input text to be reversed.");
+// When user input data and click enter key.
+standard_input.on('data', function (inputString) {
+	var reversedString = fs.reverseString(inputString);
+	console.log('Reversed string : ' + reversedString + "\n\r");
+});
+
